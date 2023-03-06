c86909b30efe57e3bb9065144190228112ebf6f1
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-06 21:22:03 +0700

    Task Praktek - Conditional Statement

---
+++ tes.js
@@ -0,0 +1,21 @@
+let cuaca = "cerah",
+adaSepatuHitam = false;
+if (cuaca){
+    console.log("Membeli Sebuah Sepatu Di Toko")
+    if(adaSepatuHitam){
+        console.log("Membeli Warna Hitam")
+    }
+    else{
+        console.log("Membeli Sepatu Warna Putih")
+        console.log("Membeli Kaus Kaki Warna Putih Juga") 
+    }
+}else {
+    console.log("Membeli Sepatu dari online")
+    if(adaSepatuHitam){
+        console.log("Membeli Warna Hitam")
+    }
+    else{
+        console.log("Membeli Sepatu Warna Putih")
+        console.log("Membeli Kaus Kaki Warna Putih Juga") 
+    }
+}
